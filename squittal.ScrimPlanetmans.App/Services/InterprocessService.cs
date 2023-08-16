cfe9c3a2c74013abac38381977074e507ea7b0e1
DJ <12602980+bigwoke@users.noreply.github.com>
2023-08-16 19:35:42 -0400

    Add service to send data upon internal events

---
+++ squittal.ScrimPlanetmans.App/Services/InterprocessService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using squittal.ScrimPlanetmans.App.SignalR.Hubs;
+using squittal.ScrimPlanetmans.ScrimMatch.Messages;
+using squittal.ScrimPlanetmans.Services.ScrimMatch;
+
+namespace squittal.ScrimPlanetmans.App.Services
+{
+    public class InterprocessService : IHostedService
+    {
+        private readonly IScrimMessageBroadcastService _messageService;
+        private readonly IHubContext<MatchDataHub, IMatchDataHub> _dataHubContext;
+
+        public InterprocessService(IScrimMessageBroadcastService messageService,
+            IHubContext<MatchDataHub, IMatchDataHub> dataHubContext)
+        {
+            _messageService = messageService;
+            _dataHubContext = dataHubContext;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _messageService.RaiseMatchStateUpdateEvent += OnMatchStateUpdate;
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        private void OnMatchStateUpdate(object sender, ScrimMessageEventArgs<MatchStateUpdateMessage> e)
+            => _dataHubContext.Clients.All.MatchStateChanged(e.Message);
+    }
+}
