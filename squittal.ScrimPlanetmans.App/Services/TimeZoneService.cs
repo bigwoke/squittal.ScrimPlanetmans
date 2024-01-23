099bd553b1b530946915dd2e700481a9fe596f72
DJ <12602980+bigwoke@users.noreply.github.com>
2024-01-22 19:57:38 -0500

    Add TimeZoneService to get client timezone via JS

---
+++ squittal.ScrimPlanetmans.App/Services/TimeZoneService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace squittal.ScrimPlanetmans.App.Services
+{
+    public class TimeZoneService
+    {
+        public const string JS_FUNC_NAME = "GetUserTimeZone";
+
+        private readonly IJSRuntime _jsRuntime;
+
+        public TimeZoneService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+            Task.Run(SetTimeZone);
+        }
+
+        public TimeZoneInfo TimeZone { get; private set; } = TimeZoneInfo.Utc;
+
+        public DateTimeOffset LocalNow => Localize(DateTime.Now);
+
+        public DateTimeOffset Localize(DateTime dateTime)
+            => TimeZoneInfo.ConvertTime(dateTime, TimeZone);
+
+        private async Task SetTimeZone()
+        {
+            string ianaTimeZone = await _jsRuntime.InvokeAsync<string>(JS_FUNC_NAME);
+
+            try
+            {
+                TimeZone = TimeZoneInfo.FindSystemTimeZoneById(ianaTimeZone);
+            }
+            catch (Exception ex) when (ex is TimeZoneNotFoundException or SecurityException or InvalidTimeZoneException)
+            {
+                TimeZone = TimeZoneInfo.Utc;
+            }
+        }
+    }
+}
--- squittal.ScrimPlanetmans.App/Startup.cs
+++ squittal.ScrimPlanetmans.App/Startup.cs
@@ -122,6 +122,8 @@ namespace squittal.ScrimPlanetmans.App
 
             services.AddSingleton<InterprocessService>();
             services.AddHostedService<InterprocessServiceStarter>();
+
+            services.AddScoped<TimeZoneService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
